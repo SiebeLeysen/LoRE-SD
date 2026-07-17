b5744e8d19f1b31122d6ba228c87d2eb647018af
sleyse4 <siebe.leysen@kuleuven.be>
2026-07-17 16:41:16 +0200

    Include toml file and update setup.py to remove unused dependencies

--- setup.py
+++ setup.py
@@ -1,43 +1,14 @@
-import os
 
-from setuptools import setup, find_packages, Extension
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup, find_packages
 
-cpp_args = ['-std=c++11']
-
-nlopt_dir = os.environ.get('NLOPT_DIR')
-nlopt_include_dir = os.environ.get('NLOPT_INCLUDE_DIR') or (
-    os.path.join(nlopt_dir, 'include') if nlopt_dir else None
-)
-nlopt_lib_dir = os.environ.get('NLOPT_LIB_DIR') or (
-    os.path.join(nlopt_dir, 'lib') if nlopt_dir else None
-)
-
-include_dirs = [d for d in (nlopt_include_dir,) if d]
-library_dirs = [d for d in (nlopt_lib_dir,) if d]
-
-
-ext_modules = [
-    Pybind11Extension(
-        'lore_sd.cpp_optimiser',
-        sources=[
-            'src/lore_sd/cpp/bindings.cpp',
-            'src/lore_sd/cpp/optimise.cpp',
-        ],
-        include_dirs=include_dirs,
-        library_dirs=library_dirs,
-        libraries=['nlopt'],
-        extra_compile_args=cpp_args,
-    ),
-]
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name='lore_sd',
-    version='0.1',
+    version='1.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    cmdclass={"build_ext": build_ext},
-    ext_modules=ext_modules,
     install_requires=[
         'numpy',
         'scipy',
@@ -52,14 +23,13 @@ setup(
     entry_points={
         'console_scripts': [
             'lore_dwi2decomposition=lore_sd.dwi2decomposition:main',
-            'lore_angular_correlation=lore_sd.angular_correlation:main',
             'lore_decomposition2contrast=lore_sd.decomposition2contrast:main',
         ],
     },
     author='Siebe Leysen',
     author_email='siebe.leysen@kuleuven.be',
     description='A package for DWI decomposition: LoRE-SD decomposes the dMRI data into voxel-level ODFs and response functions.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/SiebeLeysen/LoRE-SD',
     python_requires='>=3.6',
